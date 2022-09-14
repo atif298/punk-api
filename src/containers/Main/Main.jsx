2acd79fd58d3b3bb3bc4654b610407eccdf8c81f
atif298 <atifuddin03@gmail.com>
2022-09-14 16:43:27 +0100

    beersJSX created and props passed down to cards

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,25 +1,46 @@
 import "./Main.scss";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import BeerCard from "../../components/BeerCard/BeerCard";
 
 const Main = () => {
+  const [beers, setBeers] = useState([]);
 
-    const [beers, setBeers] = useState([]);
-
-    const getBeers = async () => {
+  const getBeers = async () => {
     const url = `https://api.punkapi.com/v2/beers`;
     const res = await fetch(url);
     const data = await res.json();
     setBeers(data);
-    };
+  };
 
-    useEffect(() => {getBeers()},[])
+  useEffect(() => {
+    getBeers();
+  }, []);
 
-    console.log(beers)
-
+  const beersJSX = (array) => {
+    return array.map((beer, index) => {
+        return (
+            <div key={index}>
+              <BeerCard
+                name={beer.name}
+                tagline={beer.tagline}
+                firstBrewed={beer.first_brewed}
+                description={beer.description}
+                img={beer.image_url}
+                abv={beer.abv}
+                ibu={beer.ibu}
+                ph={beer.ph}
+              />
+            </div>)
+    })
+  };
 
   return (
-    <></>
-  )
-}
+    <div className="main">
+        <div className="main__beers">
+            {beersJSX(beers)}
+        </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
