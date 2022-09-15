8b75482db7ccce5d131c28d0860e7060ac91b11f
atif298 <atifuddin03@gmail.com>
2022-09-15 23:03:29 +0100

    ABV and pH props passed to nav & range calibrated

--- src/containers/Nav/Nav.jsx
+++ src/containers/Nav/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import "./Nav.scss"
+import PhSlider from '../../components/PhSlider/PhSlider.jsx'
+import AbvSlider from '../../components/AbvSlider/AbvSlider.jsx'
 
-const Nav = () => {
+const Nav = ({getPhSliderValue, phValue, getAbvSliderValue, abvValue}) => {
+
+
+
   return (
-    <div>Nav</div>
+    <div className="nav">
+      <p className='nav__slider-number'>pH Level: {phValue}</p>
+      <PhSlider className="nav__slider--ph" getPhSliderValue={getPhSliderValue} setPhMax="6" setPhMin="3"/>
+      <p className='nav__slider-number'>ABV Level: {abvValue}%</p>
+      <AbvSlider className="nav__slider--abv" getAbvSliderValue={getAbvSliderValue} setAbvMax="100" setAbvMin="0"/>
+    </div>
   )
 }
 
