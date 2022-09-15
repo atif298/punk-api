308b96c3cb52c5ce5e24fe46d095f4661e635931
atif298 <atifuddin03@gmail.com>
2022-09-15 23:04:37 +0100

    pH & ABV values retrieved by and passed to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import "./App.scss";
 import Nav from "./containers/Nav/Nav";
 import Main from "./containers/Main/Main"
+import { useState } from "react";
 
 const App = () => {
 
+  const [phValue, setPhValue] = useState("6");
+  const [abvValue, setAbvValue] = useState("100");
+
+  const getPhSliderValue = (e) => {
+    setPhValue(e.target.value)};
+
+  const getAbvSliderValue = (e) => {
+    setAbvValue(e.target.value)};
+
   return (
     <div className="app">
-      <Nav />
-      <Main />
+      <Nav getPhSliderValue={getPhSliderValue} phValue={phValue} getAbvSliderValue={getAbvSliderValue} abvValue={abvValue}/>
+      <Main phValue={phValue} abvValue={abvValue}/>
     </div>
   )
 }
