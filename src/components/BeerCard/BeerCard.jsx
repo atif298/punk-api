d3d95c05b5530ff9768cefa9c90cada6c8171c42
atif298 <atifuddin03@gmail.com>
2022-09-16 13:46:29 +0100

    Beer card styled and edited

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -17,12 +17,12 @@ const BeerCard = (props) => {
     <div className="card">
         <img className="card__img" src={img}/>
         <p className="card__name">{name}</p>
-        <p className="card__tagline">{tagline}</p>
-        <p className="card__firstBrewed">First brewed: {firstBrewed}</p>
-        <p className="card__description">{description}</p>
-        <p className="card__abv">ABV: {abv}</p>
-        <p className="card__ibu">IBU: {ibu}</p>
-        <p className="card__ph" >pH: {ph}</p>
+        {/* <p className="card__tagline">{tagline}</p> */}
+        {/* <p className="card__description">{description}</p> */}
+        <p className="card__details">ABV: {abv}</p>
+        {/* <p className="card__ibu">IBU: {ibu}</p> */}
+        <p className="card__details" >pH: {ph}</p>
+        <p className="card__details">First brewed: {firstBrewed}</p>
     </div>
   );
 };
