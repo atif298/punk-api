77c97bbf8d22386b0f3dfde325708b1ceb21e0a7
atif298 <atifuddin03@gmail.com>
2022-09-16 13:49:09 +0100

    Search and slider func added & updated & styled

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -2,9 +2,9 @@ import "./Main.scss";
 import React, { useState, useEffect } from "react";
 import BeerCard from "../../components/BeerCard/BeerCard";
 
-const Main = ({phValue, abvValue}) => {
+const Main = ({searchTerm, phValue, abvValue, firstBrewedValue}) => {
+  
   const [beers, setBeers] = useState([]);
-
 
   const getBeers = async () => {
     const url = `https://api.punkapi.com/v2/beers`;
@@ -17,8 +17,15 @@ const Main = ({phValue, abvValue}) => {
     getBeers();
   }, []);
 
+  console.log(searchTerm);
   console.log(phValue);
   console.log(abvValue);
+
+  const filterBySearch = (beerArr, searchTerm) => {
+    return beerArr.filter((beer) => {
+      return(beer.name.toLowerCase().includes(searchTerm))
+    })
+  }
 
   const filterByPh = (beerArr, phValue) => {
     return beerArr.filter((beer) => {
@@ -36,18 +43,26 @@ const Main = ({phValue, abvValue}) => {
     })
   }
 
+  const filterByFirstBrewed = (beerArr, firstBrewedValue) => {
+    return beerArr.filter((beer) => {
+      if (beer.first_brewed.slice(-4) <= firstBrewedValue ) {
+        return beer
+      }
+    })
+  }
+
   const beersJSX = (beerArr) => {
-    return filterByAbv(filterByPh(beerArr, phValue), abvValue).map((beer, index) => {
+    return filterByFirstBrewed(filterByAbv(filterByPh(filterBySearch(beerArr, searchTerm.toLowerCase()), phValue), abvValue), firstBrewedValue).map((beer, index) => {
         return (
             <div key={index}>
               <BeerCard
                 name={beer.name}
-                tagline={beer.tagline}
+                //tagline={beer.tagline}
                 firstBrewed={beer.first_brewed}
-                description={beer.description}
+                //description={beer.description}
                 img={beer.image_url}
                 abv={beer.abv}
-                ibu={beer.ibu}
+                //ibu={beer.ibu}
                 ph={beer.ph}
               />
             </div>)
