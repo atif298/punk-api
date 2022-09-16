6f2dc8c3fb4ee39c7b59e7a945de7f08e66154e4
atif298 <atifuddin03@gmail.com>
2022-09-16 13:47:35 +0100

    Searchbox  passed to nav and styled

--- src/containers/Nav/Nav.jsx
+++ src/containers/Nav/Nav.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import "./Nav.scss"
-import PhSlider from '../../components/PhSlider/PhSlider.jsx'
-import AbvSlider from '../../components/AbvSlider/AbvSlider.jsx'
+import React from "react";
+import "./Nav.scss";
+import SearchBox from "../../components/SearchBox/SearchBox.jsx";
+import PhSlider from "../../components/PhSlider/PhSlider.jsx";
+import AbvSlider from "../../components/AbvSlider/AbvSlider.jsx";
+import FirstBrewedSlider from "../../components/FirstBrewedSlider/FirstBrewedSlider.jsx";
+import appLogo from "../../assets/images/appLogo.png"
 
-const Nav = ({getPhSliderValue, phValue, getAbvSliderValue, abvValue}) => {
-
-
-
+const Nav = ({
+  getFirstBrewedSliderValue,
+  firstBrewedValue,
+  getSearchBoxValue,
+  getPhSliderValue,
+  phValue,
+  getAbvSliderValue,
+  abvValue,
+}) => {
   return (
     <div className="nav">
-      <p className='nav__slider-number'>pH Level: {phValue}</p>
-      <PhSlider className="nav__slider--ph" getPhSliderValue={getPhSliderValue} setPhMax="6" setPhMin="3"/>
-      <p className='nav__slider-number'>ABV Level: {abvValue}%</p>
-      <AbvSlider className="nav__slider--abv" getAbvSliderValue={getAbvSliderValue} setAbvMax="100" setAbvMin="0"/>
+      <img src={appLogo} className="nav__logo" alt="Brewdog Logo" />
+      <SearchBox className="nav__searchbox" getSearchBoxValue={getSearchBoxValue} />
+      <p className="nav__slider-name">pH Level: 3 - {phValue}</p>
+      <PhSlider
+        className="nav__slider--ph"
+        getPhSliderValue={getPhSliderValue}
+        setPhMax="6"
+        setPhMin="3"
+      />
+      <p className="nav__slider-name">ABV Level: 0 - {abvValue}%</p>
+      <AbvSlider
+        className="nav__slider--abv"
+        getAbvSliderValue={getAbvSliderValue}
+        setAbvMax="100"
+        setAbvMin="0"
+      />
+      <p className="nav__slider-name">First Brewed: {firstBrewedValue}</p>
+      <FirstBrewedSlider
+        className="nav__slider--first-brewed"
+        getFirstBrewedSliderValue={getFirstBrewedSliderValue}
+        setFirstBrewedMax="2015"
+        setFirstBrewedMin="2007"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
