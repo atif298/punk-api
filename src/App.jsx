856dbcd41a35757d62dde4845c24c66d56eb0e03
atif298 <atifuddin03@gmail.com>
2022-09-16 13:48:01 +0100

    App styled and search box value retrieved

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
 import "./App.scss";
-import Nav from "./containers/Nav/Nav";
-import Main from "./containers/Main/Main"
+import Nav from "./containers/Nav/Nav.jsx";
+import Main from "./containers/Main/Main.jsx";
 import { useState } from "react";
 
 const App = () => {
-
+  const [searchTerm, setSearchTerm] = useState("");
   const [phValue, setPhValue] = useState("6");
   const [abvValue, setAbvValue] = useState("100");
+  const [firstBrewedValue, setFirstBrewedValue] = useState("2015");
+
+  const getSearchBoxValue = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   const getPhSliderValue = (e) => {
-    setPhValue(e.target.value)};
+    setPhValue(e.target.value);
+  };
 
   const getAbvSliderValue = (e) => {
-    setAbvValue(e.target.value)};
+    setAbvValue(e.target.value);
+  };
+
+  const getFirstBrewedSliderValue = (e) => {
+    setFirstBrewedValue(e.target.value);
+  };
 
   return (
     <div className="app">
-      <Nav getPhSliderValue={getPhSliderValue} phValue={phValue} getAbvSliderValue={getAbvSliderValue} abvValue={abvValue}/>
-      <Main phValue={phValue} abvValue={abvValue}/>
+      <Nav className="nav"
+        getSearchBoxValue={getSearchBoxValue}
+        getPhSliderValue={getPhSliderValue}
+        phValue={phValue}
+        getAbvSliderValue={getAbvSliderValue}
+        abvValue={abvValue}
+        getFirstBrewedSliderValue={getFirstBrewedSliderValue}
+        firstBrewedValue={firstBrewedValue}
+      />
+      <Main className="main"
+        searchTerm={searchTerm}
+        phValue={phValue}
+        abvValue={abvValue}
+        firstBrewedValue={firstBrewedValue}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
